37633a82c0cf207a62f9c704c2091abd46f128d5
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-18 17:31:00 +0000

    added factorialize

--- factorialize.c
+++ factorialize.c
@@ -3,23 +3,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int factorialize(long long int num);
+long long factorialize(long long num);
 
 int main(void)
 {
-    return factorialize(5);
+    // int factor = get_int("What is the factor: ");
+
+    // factorialize(20);
+    printf("factorial: %llu\n", factorialize(20));
 }
 
-int factorialize(long long num)
+// int factorialize(long long num)
+long long factorialize(long long num)
 {
-    long long answer = 0;
-    for ( int startingDigit = num; startingDigit > 0; startingDigit--)
+    long long answer = 1;
+    for (long long i = 1; i <= num; i++)
     {
-        return num *= startingDigit;
-        answer = startingDigit;
-        printf("%lli", num);
+        answer *= i;
     }
-    return answer;
-  printf("%lli", answer);
+        return answer;
+    // if (factor==0)
+
+    //     {
+    //         return 1;
+
+    //     }
+
+    //     return factor * factorialize(factor-1);
+    //     printf("%lld", factor);
+
+// //    long long answer = 0;
+// //     for ( int startingDigit = num; startingDigit < 0; startingDigit--)
+// //     {
+// //         return num *= startingDigit;
+// //         answer = startingDigit;
+// //         printf("%lli", num);
+// //     }
+// //     return 1;
+// //   printf("%lli", answer);
 }
 
